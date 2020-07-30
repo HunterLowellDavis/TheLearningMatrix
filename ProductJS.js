aec5336df474d879f9ee43e475a8fe2eb27e96f0
Karina Vasudeva <karinav@ad.unc.edu>
2020-07-30 15:28:25 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/HunterLowellDavis/TheLearningMatrix""
    This reverts commit 358031d9d8c553da5f884de792228acda1c28612.

--- ProductJS.js
+++ ProductJS.js
@@ -1,28 +1,59 @@
+(async () => {
 var path1 = "";
 var path2 = "";
 var path3 = "";
 
+var directory = "";
+await fetch("TextFiles/directory.txt")
+.then(response => response.text())
+.then(text => directory = text);
+
+console.log(directory);
+fileNames = directory.split("\n");
+console.log(fileNames);
+files = [];
+for(var i = 0; i<fileNames.length; ++i){
+    var fileName = "TextFiles/"+fileNames[i]+".txt";
+    files.push(fileName);
+    console.log("It ran");
+}
+
+console.log(files.toString());
 var tax = sessionStorage.getItem('Taxonomy');
-path1 = "TextFiles/"+tax+".txt";
-console.log(path1);
 
 var size = sessionStorage.getItem('Size');
 if(size == '&lt;15'){size = 'u15';}
-path2 = "TextFiles/"+size+".txt";
-console.log(path2);
 
 var collab = sessionStorage.getItem('Collaboration');
-path3 = "TextFiles/"+collab+".txt";
-console.log(path3);
 
-fetch(path1)
-  .then(response => response.text())
-  .then(text => document.getElementById('sec1').innerHTML = text);
+for(var i = 0; i<files.length; ++i){
+    var fileText 
+    await fetch(files[i])
+    .then(response => response.text())
+    .then(text => fileText = text);
 
-  fetch(path2)
-  .then(response => response.text())
-  .then(text => document.getElementById('sec2').innerHTML = text);
+    lineSplit = fileText.split("\n")
+    firstLine = lineSplit[0];
+    titleLine = lineSplit[1];
 
-  fetch(path3)
-  .then(response => response.text())
-  .then(text => document.getElementById('sec3').innerHTML = text);
+    tags = lineSplit[0].split(" ");
+    console.log(tags.toString());
+    if(tags.includes(tax) || tags.includes(collab) || tags.includes(size)){
+        sec = document.createElement("div");
+        sec.classList.add("productText");
+        content = lineSplit.splice(2, 100);
+        console.log(content);
+        title = document.createElement("h1");
+        title.innerHTML = titleLine;
+        sec.appendChild(title);
+        for(var j = 0; j < content.length; j++){
+            par = document.createElement("p");
+            par.innerHTML = content[j];
+            sec.appendChild(par);
+
+        }
+        console.log(titleLine+" was added");
+        document.body.insertBefore(sec, document.getElementById("bottom"));
+    }
+}
+})();
